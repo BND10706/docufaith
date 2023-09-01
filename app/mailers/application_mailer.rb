f9b119739fd0a03536c905dd759c1ac9e7e10750
Brandon.DeVore <Brandon.DeVore@publix.com>
2023-09-01 09:16:06 -0400

    Modified Docufaith content

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'DocuSeal <hi@docuseal.co>'
+  default from: 'DocuFaith'
   layout 'mailer'
 
   register_interceptor ActionMailerConfigsInterceptor
